71f2ab3333b4458fb0be0bbb0b3015cabc5e5810
desmaisons_david <desmaisons_david@hotmail.com>
2017-06-16 23:34:47 -0300

    new vesrion + build fix

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,7 +10,7 @@ var env = config.build.env
 
 var webpackConfig = merge(baseWebpackConfig, {
   entry: {
-    "VueSemanticModal": './src/VueSemanticModal.js'
+    "VueSemanticModal": './src/VueSemanticModal.vue'
   },
   module: {
     loaders: utils.styleLoaders({ sourceMap: config.build.productionSourceMap, extract: true })
